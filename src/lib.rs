86bb9bc195d6fd5d13edb48ca4a4e1fb5cc71a87
Antoine De Gendt <adegendt@student.ethz.ch>
2022-06-29 20:56:23 +0200

    Implement generic testing for heaps

--- src/lib.rs
+++ src/lib.rs
@@ -6,20 +6,24 @@ mod heap {
         pub elements: Vec<T>,
     }
 
-    pub trait BinaryHeap<T> {
-        fn new(size: usize) -> Self;
-        fn insert(&mut self, value: T);
-        fn pop(&mut self) -> T;
-    }
-
-    impl<T: std::cmp::PartialOrd + Copy> BinaryHeap<T> for Heap<T> {
-        fn new(size: usize) -> Self {
+    impl<T> Heap<T> {
+        pub fn new(size: usize) -> Self {
             Heap {
                 elements: Vec::with_capacity(size),
             }
         }
+    }
 
-        fn insert(&mut self, value: T) {
+    pub struct BinaryHeap;
+    pub struct FourAryHeap;
+
+    pub trait HeapTrait<T, U> {
+        fn insert(&mut self, value: U);
+        fn pop(&mut self) -> U;
+    }
+
+    impl<U: std::cmp::PartialOrd + Copy> HeapTrait<BinaryHeap, U> for Heap<U> {
+        fn insert(&mut self, value: U) {
             fn heapify_up<T: std::cmp::PartialOrd>(heap: &mut Heap<T>, i: usize) {
                 let mut cur_elem = i;
                 let mut parent: usize = cur_elem.wrapping_sub(1) / 2;
@@ -34,8 +38,58 @@ mod heap {
             self.elements.push(value);
             heapify_up(self, self.elements.len() - 1);
         }
-        fn pop(&mut self) -> T {
-            fn heapify_down<T: std::cmp::PartialOrd>(heap: &mut Heap<T>, i: usize) {
+
+        fn pop(&mut self) -> U {
+            fn heapify_down<U: std::cmp::PartialOrd>(heap: &mut Heap<U>, i: usize) {
+                let mut i = i;
+                let mut left_child = 2 * i + 1;
+                let mut right_child = 2 * i + 2;
+                while left_child < heap.elements.len() {
+                    let mut largest = left_child;
+                    if right_child < heap.elements.len()
+                        && heap.elements[right_child] > heap.elements[left_child]
+                    {
+                        largest = right_child;
+                    }
+                    if heap.elements[largest] > heap.elements[i] {
+                        heap.elements.swap(largest, i);
+                        i = largest;
+                        left_child = 2 * i + 1;
+                        right_child = 2 * i + 2;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            let max = self.elements[0];
+            let cur_len = self.elements.len();
+            self.elements.swap(0, cur_len - 1);
+            self.elements.pop();
+            heapify_down(self, 0);
+            max
+        }
+    }
+
+    impl<U: std::cmp::PartialOrd + Copy> HeapTrait<FourAryHeap, U> for Heap<U> {
+        fn insert(&mut self, value: U) {
+            fn heapify_up<T: std::cmp::PartialOrd>(heap: &mut Heap<T>, i: usize) {
+                let mut cur_elem = i;
+                let mut parent: usize = cur_elem.wrapping_sub(1) / 2;
+                while parent < heap.elements.len()
+                    && heap.elements[cur_elem] > heap.elements[parent]
+                {
+                    heap.elements.swap(cur_elem, parent);
+                    cur_elem = parent;
+                    parent = cur_elem.wrapping_sub(1) / 2;
+                }
+            }
+            self.elements.push(value);
+            heapify_up(self, self.elements.len() - 1);
+            panic!("Ahhhhhhhhhhhhhhhhhh!");
+        }
+
+        fn pop(&mut self) -> U {
+            fn heapify_down<U: std::cmp::PartialOrd>(heap: &mut Heap<U>, i: usize) {
                 let mut i = i;
                 let mut left_child = 2 * i + 1;
                 let mut right_child = 2 * i + 2;
--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,7 @@
 use crate::heap::BinaryHeap;
+use crate::heap::FourAryHeap;
 use crate::heap::Heap;
+use crate::heap::HeapTrait;
 
 use ordered_float::OrderedFloat;
 use rand::distributions::Standard;
@@ -10,18 +12,19 @@ static TEST_SIZE: usize = 100_000;
 
 use rand::Rng;
 #[rstest]
-#[case::heap(BinaryHeap::<OrderedFloat<f64>>::new(TEST_SIZE))]
-#[case::heap(BinaryHeap::<u64>::new(TEST_SIZE))]
-fn binary_heap<T>(#[case] mut heap: Heap<T>)
+#[case(Heap::<OrderedFloat<f64>>::new(TEST_SIZE), BinaryHeap)]
+#[case(Heap::<u64>::new(TEST_SIZE), FourAryHeap)]
+fn binary_heap<T, U>(#[case] mut heap: Heap<U>, #[case] _implementation: T)
 where
-    T: std::cmp::PartialOrd + Copy + std::fmt::Debug + std::cmp::Ord,
-    Standard: Distribution<T>,
+    U: std::cmp::PartialOrd + Copy + std::fmt::Debug + std::cmp::Ord,
+    Standard: Distribution<U>,
+    Heap<U>: HeapTrait<T, U>,
 {
     let mut std_heap = std::collections::BinaryHeap::new();
     let mut rng = rand::thread_rng();
 
     for _x in 0..TEST_SIZE {
-        let value = rng.gen::<T>();
+        let value = rng.gen::<U>();
         heap.insert(value);
         std_heap.push(value);
         assert_eq!(heap.elements[0], *std_heap.peek().unwrap());
